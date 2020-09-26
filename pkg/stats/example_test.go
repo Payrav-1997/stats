82d51af9d2ae58caecf641e851f7e836b8678138
Payrav <payrav-343@mail.ru>
2020-09-27 00:07:29 +0500

    Добаил пробел

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -1,61 +1,62 @@
 package stats
 
 import (
-	"github.com/Payrav-1997/bank/v2/pkg/types"
 	"fmt"
+
+	"github.com/Payrav-1997/bank/v2/pkg/types"
 )
 
 func ExampleAvg() {
 	payments := []types.Payment{
-	  {
-		ID:       1,
-		Amount:   53_00,
-		Category: "Cafe",
-		Status:   types.StatusOk,     
-	  },
-	  {
-		ID:       2,
-		Amount:   51_00,
-		Category: "Cafe",
-		Status:   types.StatusOk,
-	  },
-	  {
-		ID:       3,
-		Amount:   52_00,
-		Category: "Cafe",
-		Status:   types.StatusFail,
-	  },
+		{
+			ID:       2,
+			Amount:   53_00,
+			Category: "Cafe",
+			Status:   types.StatusOk,
+		},
+		{
+			ID:       2,
+			Amount:   51_00,
+			Category: "Cafe",
+			Status:   types.StatusOk,
+		},
+		{
+			ID:       1,
+			Amount:   52_00,
+			Category: "Cafe",
+			Status:   types.StatusFail,
+		},
 	}
-	
-    fmt.Println(Avg(payments))
+
+	fmt.Println(Avg(payments))
 	//Output: 5200
 }
-  
-  func ExampleTotalInCategory() {
+
+func ExampleTotalInCategory() {
 	payments := []types.Payment{
-	  {
-		ID:       1,
-		Amount:   10_000_00,
-		Category: "cafe",
-		Status:   types.StatusOk,
-	  },
-	  {
-		ID:       2,
-		Amount:   20_000_00,
-		Category: "Home",
-		Status:   types.StatusOk,
-	  },
-	  {
-		ID:       3,
-		Amount:   30_000_00,
-		Category: "restaurant",
-		Status:   types.StatusFail,
-	  },
+		{
+			ID:       1,
+			Amount:   10_000_00,
+			Category: "cafe",
+			Status:   types.StatusOk,
+		},
+		{
+			ID:       2,
+			Amount:   20_000_00,
+			Category: "Home",
+			Status:   types.StatusOk,
+		},
+		{
+			ID:       3,
+			Amount:   30_000_00,
+			Category: "restaurant",
+			Status:   types.StatusFail,
+		},
 	}
-  
+
 	result := types.Category("cafe")
 	totalInCategory := TotalInCategory(payments, result)
 	fmt.Println(totalInCategory)
 	//Output:  1000000
-  
-  }
+
+}
