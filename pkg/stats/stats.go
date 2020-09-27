e8c20d553f63918940f943edd8ed9340f075a79b
Payrav <payrav-343@mail.ru>
2020-09-27 23:11:21 +0500

    Добавил stats_test.go и зменил функцию в stats.go

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -4,31 +4,19 @@ import (
 	"github.com/Payrav-1997/bank/v2/pkg/types"
 )
 
-//Avg рассчитает среднюю сумму
-func Avg(payments []types.Payment) types.Money {
+//CategoriesAvg считает среднюю сумму платежа по каждой категории
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	category := map[types.Category]types.Money{}
+	categories := map[types.Category]types.Money{}
 
-	var sum = types.Money(0)
-	var i = 0
-	for _, card := range payments {
-		if card.Status != types.StatusFail {
-			sum += card.Amount
-			i++
-		}
-	}
-	return sum / types.Money(i)
-}
-
-//TotalInCategory находит сумму в определенной категории
-func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
-	var t = types.Money(0)
-
-	for _, card := range payments {
-		if card.Status != types.StatusFail {
-			if category == card.Category {
-				t += card.Amount
-			}
-		}
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+		category[payment.Category]++
 	}
 
-	return t
+	for key := range categories {
+		categories[key] /= category[key]
+	}
+
+	return categories
 }
