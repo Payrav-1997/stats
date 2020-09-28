c3403ac60d42b33bf49aa87a4a7325823d00b0b2
Payrav <payrav-343@mail.ru>
2020-09-28 21:39:15 +0500

    Добавил функцию PeriodsDynamic и тетсты

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -60,3 +60,94 @@ func TestCategoriesAvg_one(t *testing.T) {
 		t.Errorf("\n got > %v want > nil", result)
 	}
 }
+
+
+
+func TestPeriodsDynamic_positive(t *testing.T) {
+	one := map[types.Category]types.Money{
+		"auto": 14,
+		"food": 20,		
+	}
+	second := map[types.Category]types.Money{
+		"auto": 17,
+		"food": 35,
+	}
+	want := map[types.Category]types.Money{
+		"auto": 3,
+		"food": 15,
+	}
+
+	got := PeriodsDynamic(one, second)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("\n got > %v \n want > %v", got, want)
+	}
+
+}
+
+func TestPeriodsDynamic_negative(t *testing.T) {
+	one := map[types.Category]types.Money{
+		"auto": 14,
+		"food": 20,
+	}
+	second := map[types.Category]types.Money{
+		"auto": 7,
+		"food": 15,
+	}
+	want := map[types.Category]types.Money{
+		"auto": -7,
+		"food": -5,
+	}
+
+	got := PeriodsDynamic(one, second)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("\n got > %v \n want > %v", got, want)
+	}
+
+}
+
+func TestPeriodsDynamic_notEqualMap(t *testing.T) {
+	one := map[types.Category]types.Money{
+		"auto": 14,
+		"food": 20,
+		
+	}
+	second := map[types.Category]types.Money{
+		"food": 35,
+	}
+	want := map[types.Category]types.Money{
+		"auto": -14,
+		"food": 15,
+	}
+
+	got := PeriodsDynamic(one, second)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("\n got > %v \n want > %v", got, want)
+	}
+
+}
+
+func TestPeriodsDynamic_OneMoreElem(t *testing.T) {
+	one := map[types.Category]types.Money{
+		"auto": 14,
+		"food": 20,		
+	}
+	second := map[types.Category]types.Money{
+		"auto":   17,
+		"food":   35,
+		"mobile": 17,
+	}
+	want := map[types.Category]types.Money{
+		"auto":   3,
+		"food":   15,
+		"mobile": 17,
+	}
+
+	got := PeriodsDynamic(one, second)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("\n got > %v \n want > %v", got, want)
+	}
+}
